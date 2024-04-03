9eaab09eb7d10622743521f6529ac26bd337ea5f
Josh Innes <jinnes@pulsion.co.uk>
2024-04-03 13:43:04 +0100

    squash

--- AutomataVideoGenerator/AutomataVideoGenerator/Program.cs
+++ AutomataVideoGenerator/AutomataVideoGenerator/Program.cs
@@ -1,11 +1,10 @@
 ﻿using AutomataVideoGenerator.Automatons;
 using AutomataVideoGenerator.Automatons.GPUCompute;
-using AutomataVideoGenerator.Automatons.Standard;
 using System.Drawing;
 
 //new BoostedGameOfLife(100, 100).run(50, "BoostedGOLResults", 10);
 //new BoostedBugs(100, 100).run(50, "BoostedBUGSResults", 10);
 
-new GenericBSN(100, 100, GenericBSN.defaults.GameOfLife).run(50, "BoostedGOLResults", 10);
-new GenericBSN(100, 100, GenericBSN.defaults.Bugs).run(50, "BoostedBugsResults", 10);
+new GenericBSN(100, 100, GenericBSN.defaults.GameOfLife).run(50, "GOLResults", 10);
+new GenericBSN(100, 100, GenericBSN.defaults.Bugs).run(50, "BugsResults", 10);
 
