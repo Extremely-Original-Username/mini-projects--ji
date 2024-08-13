bf592d8b2efbebc3d94eed179841ab86ee1974d3
Josh Innes <jinnes@pulsion.co.uk>
2024-08-13 16:14:40 +0100

    added metabolism and death

--- Critters/Model/Objects/Critter.cs
+++ Critters/Model/Objects/Critter.cs
@@ -9,16 +9,48 @@ namespace Model.Objects
 {
     public class Critter : Agent
     {
+        public bool Dead { get; private set; } = false;
+
+        private readonly float maxEnergy;
+        private float energy;
+        private float metabolicRate;
+
         public Critter(World world, Vector2<int> position, Vector2<int> size, Vector2<float> facingAngle) : base(world, position, size, facingAngle)
         {
+            float sizeScalar = size.X * size.Y;
+            maxEnergy = sizeScalar;
+            metabolicRate = (float)Math.Pow(sizeScalar, 0.75f) / 100; //Divisor of 10 = 30 frame starvation for 100 maxEnergy, 100 = 300 - TODO make parameter
 
+            energy = maxEnergy / 2 + (new Random().Next(40) - 20); //Make these parameters too
         }
 
         public override void OnUpdate()
         {
             base.OnUpdate();
+
+            if (Dead) return;
+
+            metabolise();
+            if (energy > maxEnergy * 0.7) tryReproduce();
+            else if (energy <= 0) die();
+
             Move();
             Rotate();
+        }
+
+        private void metabolise()
+        {
+            energy -= metabolicRate;
+        }
+
+        private void tryReproduce()
+        {
+
+        }
+
+        private void die()
+        {
+            this.Dead = true;
         }
 
         private void Move()
@@ -27,7 +59,7 @@ namespace Model.Objects
             float cX = FacingAngle.X;
             float cY = FacingAngle.Y;
 
-            int dX = r.NextSingle() < Math.Abs(cX)? (int)Math.Clamp(cX * 1000, -1, 1) : 0;
+            int dX = r.NextSingle() < Math.Abs(cX) ? (int)Math.Clamp(cX * 1000, -1, 1) : 0;
             int dY = r.NextSingle() < Math.Abs(cY) ? (int)Math.Clamp(cY * 1000, -1, 1) : 0;
 
             Translate(dX, dY);
